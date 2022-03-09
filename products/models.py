f5c42403753f7244baa94637d92610f1f0b44392
Brainiac Rawkib <brainiacrawkib@gmail.com>
2022-03-09 12:40:11 -0800

    added available field

--- products/models.py
+++ products/models.py
@@ -16,6 +16,7 @@ class Product(models.Model):
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
     quantity = models.PositiveIntegerField()
+    available = models.BooleanField(default=True)
     created_at = models.DateTimeField(default=now)
     updated_at = models.DateTimeField(auto_now=True)
 
