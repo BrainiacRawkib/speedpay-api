9be783698bc7c6e2be706695d00b47f5c58c223f
Brainiac Rawkib <brainiacrawkib@gmail.com>
2022-03-09 13:04:07 -0800

    added products urls in main urls

--- speedpay/urls.py
+++ speedpay/urls.py
@@ -7,6 +7,7 @@ from users.views import LoginAPIView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/users/', include('users.urls')),
+    path('api/products/', include('products.urls')),
     path('api/login/', LoginAPIView.as_view()),
     path('api/token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
