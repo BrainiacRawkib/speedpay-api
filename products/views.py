504e17eb3df2f81e89dd2e6a3717ea6fa6bbe101
Brainiac Rawkib <brainiacrawkib@gmail.com>
2022-03-09 14:12:06 -0800

    modified codebase

--- products/views.py
+++ products/views.py
@@ -1,6 +1,8 @@
 from apiutils.views import http_response, validate_keys
+from apiutils.error_codes import ErrorCodes
 from rest_framework.views import APIView
 from rest_framework import status
+from users.utils import get_user_access_token
 from .serializers import CategorySerializer, ProductSerializer
 
 
@@ -24,7 +26,21 @@ class ProductAPIView(APIView):
         pass
 
     def post(self, request, *args, **kwargs):
-        pass
+        token = request.headers.get('Access-Token')
+        if not token:
+            return http_response(
+                'Access Token missing',
+                status=status.HTTP_400_BAD_REQUEST,
+                data=ErrorCodes.INVALID_PAYLOAD
+            )
+        user = get_user_access_token(token)
+
+        if not user:
+            return http_response(
+                'Session Timed out.',
+                status=status.HTTP_408_REQUEST_TIMEOUT
+            )
+
 
     def put(self, request, *args, **kwargs):
         pass
