37c64b070c8d3c03e3132d7241bd4f0be8530b7e
Brainiac Rawkib <brainiacrawkib@gmail.com>
2022-03-09 10:24:00 -0800

    modified code field

--- users/models.py
+++ users/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
+    code = models.CharField(max_length=20, unique=True)
     email = models.EmailField(unique=True)
     is_deleted = models.BooleanField(default=False)
 
