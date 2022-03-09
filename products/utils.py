17a579d3bc40ec60e2db48567773e5cc23993e72
Brainiac Rawkib <brainiacrawkib@gmail.com>
2022-03-09 12:04:27 -0800

    created create_category(), create_product()

---
+++ products/utils.py
@@ -0,0 +1,30 @@
+"""ABSTRACTING DB OPERATIONS FROM VIEWS."""
+from apiutils.utils import logger, generate_code
+from .models import Category, Product
+
+
+"""CRUD OPERATIONS"""
+
+"""CREATE"""
+def create_category(name):
+    try:
+        pass
+
+    except Exception as e:
+        logger.error("create_category@Error")
+        logger.error(e)
+        return None
+
+
+def create_product(name):
+    try:
+        pass
+
+    except Exception as e:
+        logger.error("create_product@Error")
+        logger.error(e)
+        return None
+
+"""RETRIEVE"""
+"""UPDATE"""
+"""DELETE"""
