f2096f44584ab6098422c89688c9696119140a0d
Brainiac Rawkib <brainiacrawkib@gmail.com>
2022-03-09 11:22:00 -0800

    modified UserAPIView.get()

--- users/views.py
+++ users/views.py
@@ -1,5 +1,7 @@
 from apiutils.views import http_response
+from apiutils.error_codes import ErrorCodes
 from rest_framework.views import APIView
+from rest_framework import status
 from .serialiazers import UserSerializer
 from .utils import get_user, get_all_users
 
@@ -8,6 +10,14 @@ class UserAPIView(APIView):
 
     def get(self, request, *args, **kwargs):
         payload = request.data
+        token = payload['access']
+
+        if not token:
+            return http_response(
+                'Access Token missing',
+                status=status.HTTP_400_BAD_REQUEST,
+                error_code=ErrorCodes.INVALID_PAYLOAD
+            )
 
     def post(self, request, *args, **kwargs):
         pass
