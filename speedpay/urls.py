d0dec0e752f1636fbc23a52847e80a154089911d
Brainiac Rawkib <brainiacrawkib@gmail.com>
2022-03-09 11:43:23 -0800

    modified main urls

--- speedpay/urls.py
+++ speedpay/urls.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework_simplejwt import views as jwt_views
+from users.views import LoginAPIView
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/users/', include('users.urls')),
+    path('api/login/', LoginAPIView.as_view()),
     path('api/token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
 ]
