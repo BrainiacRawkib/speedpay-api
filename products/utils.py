63774e8a3a0f2602ea8b089ac5e6e62e34af54fc
Brainiac Rawkib <brainiacrawkib@gmail.com>
2022-03-09 12:19:29 -0800

    modified create_product()

--- products/utils.py
+++ products/utils.py
@@ -1,5 +1,6 @@
 """ABSTRACTING DB OPERATIONS FROM VIEWS."""
 from apiutils.utils import logger, generate_code
+from .constraint_checks import check_category_name, check_product_details
 from .models import Category, Product
 
 
@@ -8,6 +9,8 @@ from .models import Category, Product
 """CREATE"""
 def create_category(name):
     try:
+        if check_category_name(name=name):
+            return None
         return Category.objects.create(name=name)
 
     except Exception as e:
@@ -16,9 +19,18 @@ def create_category(name):
         return None
 
 
-def create_product(name):
+def create_product(category, title, price, quantity):
     try:
-        pass
+        if check_product_details(title=title):
+            return None
+        return Product.objects.create(
+            code=generate_code('products', 'Product'),
+            category=category,
+            title=title,
+            price=price,
+            quantity=quantity
+
+        )
 
     except Exception as e:
         logger.error("create_product@Error")
