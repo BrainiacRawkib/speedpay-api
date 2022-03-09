942aaa2e80f3bf02837576db910811c5f032b2af
Brainiac Rawkib <brainiacrawkib@gmail.com>
2022-03-09 12:39:34 -0800

    created get_category(), get_categories(), get_product(), get_all_products(), get_products_by_category(), update_product(), delete_product()

--- products/utils.py
+++ products/utils.py
@@ -38,5 +38,94 @@ def create_product(category, title, price, quantity):
         return None
 
 """RETRIEVE"""
+
+def get_category(name):
+    """Get a specific category."""
+    try:
+        return Category.objects.get(name=name)
+
+    except Exception as e:
+        logger.error('get_category@Error')
+        logger.error(e)
+        return None
+
+
+def get_categories():
+    """Get all categories."""
+    try:
+        return Category.objects.all()
+
+    except Exception as e:
+        logger.error('get_categories@Error')
+        logger.error(e)
+        return []
+
+
+def get_product(*args, **kwargs):
+    """Get product by title or code."""
+    try:
+        if kwargs['title']:
+            return Product.objects.get(title=kwargs['title'])
+        if kwargs['code']:
+            return Product.objects.get(code=kwargs['code'])
+        return None
+
+    except Exception as e:
+        logger.error('get_product@Error')
+        logger.error(e)
+        return None
+
+
+def get_all_products():
+    """Get all products."""
+    try:
+        return Product.objects.all()
+
+    except Exception as e:
+        logger.error('get_all_products@Error')
+        logger.error(e)
+        return []
+
+
+def get_products_by_category(category=None):
+    """Get products by specified category."""
+    try:
+        if category:
+            return Product.objects.filter(category=category)
+        return get_all_products()
+
+    except Exception as e:
+        logger.error('get_products_by_category@Error')
+        logger.error(e)
+        return []
+
 """UPDATE"""
+
+def update_product(product, validated_data):
+    """Update a product."""
+    try:
+        product.price = validated_data.get('price', product.price)
+        product.quantity = validated_data.get('quantity', product.quantity)
+        product.description = validated_data.get('description', product.description)
+        product.save()
+        return product
+
+    except Exception as e:
+        logger.error('update_product@Error')
+        logger.error(e)
+        return None
+
+
 """DELETE"""
+
+def delete_product(product):
+    """Delete a product."""
+    try:
+        product.available = False
+        product.save()
+        return product
+
+    except Exception as e:
+        logger.error('delete_product@Error')
+        logger.error(e)
+        return None
