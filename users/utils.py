4daaf0caf9a71b949c6b5a172d00df11cab761b4
Brainiac Rawkib <brainiacrawkib@gmail.com>
2022-03-09 10:36:39 -0800

    created get_user(), get_all_users(), delete_user()

--- users/utils.py
+++ users/utils.py
@@ -24,8 +24,48 @@ def create_user(first_name, last_name, username, email):
         logger.error(e)
         return None
 
+
 """RETRIEVE"""
+def get_user(*args, **kwargs):
+    """Retrieve a User by keyword args: username, code, email, or contact"""
+    try:
+        if kwargs['username']:
+            return User.objects.get(username=kwargs['username'])
+        elif kwargs['code']:
+            return User.objects.get(code=kwargs['code'])
+        elif kwargs['email']:
+            return User.objects.get(email=kwargs['email'])
+        else:
+            return None
+
+    except Exception as e:
+        logger.error('get_user@Error')
+        logger.error(e)
+        return None
+
+
+def get_all_users():
+    try:
+        return User.objects.all()
+
+    except Exception as e:
+        logger.error('get_all_users@Error')
+        logger.error(e)
+        return []
 
 """UPDATE"""
 
 """DELETE"""
+
+def delete_user(user):
+    """Delete a user."""
+    try:
+        user.is_deleted = True
+        user.is_active = False
+        user.save()
+        return True
+
+    except Exception as e:
+        logger.error('delete_user@Error')
+        logger.error(e)
+        return False
