2fa28d42ab9a8435c464890491d284a9f4cfc52e
Brainiac Rawkib <brainiacrawkib@gmail.com>
2022-03-09 10:28:31 -0800

    created create_user()

--- users/utils.py
+++ users/utils.py
@@ -1,6 +1,28 @@
 """ABSTRACTING DB OPERATIONS FROM VIEWS."""
-from apiutils.utils import logger
+from apiutils.utils import logger, generate_code
 from .models import User
 
 
-""""""
+"""CRUD OPERATIONS"""
+
+"""CREATE"""
+def create_user(first_name, last_name, username, email):
+    try:
+        return User.objects.create_user(
+            code=generate_code('users', 'User'),
+            first_name=first_name,
+            last_name=last_name,
+            username=username,
+            email=email
+        )
+
+    except Exception as e:
+        logger.error('create_user@Error')
+        logger.error(e)
+        return None
+
+"""RETRIEVE"""
+
+"""UPDATE"""
+
+"""DELETE"""
