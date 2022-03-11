dfd6cc9ba82ea57aa59ba55271679d43012d6e3c
Brainiac Rawkib <brainiacrawkib@gmail.com>
2022-03-10 16:43:51 -0800

    modified DATABASES settings

--- speedpay/settings.py
+++ speedpay/settings.py
@@ -86,16 +86,16 @@ WSGI_APPLICATION = 'speedpay.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
-DATABASES = {'default': dj_database_url.config(
-    default=os.getenv('CLEARDB_DATABASE_URL'),
-)}
+# DATABASES = {'default': dj_database_url.config(
+#     default=os.getenv('CLEARDB_DATABASE_URL'),
+# )}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
